9b5924241e8bf5f8bb5be314159c5a0be5e344ac
23521114-NguyenTruongYenNhi <23521114@gm.uit.edu.vn>
2025-12-07 00:35:52 +0700

    Fix authroute

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
-import express from 'express';
-import { signup, login } from '../controllers/authController.js';
+﻿import express from 'express';
+// Sửa 'signup' thành 'register' để khớp với file controller
+import { register, login } from '../controllers/authController.js';
 
 const router = express.Router();
 
-router.post('/signup', signup);
+// Đường dẫn vẫn là /signup, nhưng gọi hàm xử lý là register
+router.post('/signup', register);
 router.post('/login', login);
 
 export default router;
